566e2bd1928d2947e009f12c92d5b939ac818d85
Novitasari Arlim <42811785+aquemos@users.noreply.github.com>
2023-06-14 06:26:47 +0700

    Add transaction and script code file
    Script.py is the main code and transaction.py contain class transaction and all method used in transaction process

---
+++ script.py
@@ -0,0 +1,221 @@
+from transaction import Transaction
+import sys
+
+MENU = {
+    '1': 'Add Item',
+    '2': 'Update Item Name',
+    '3': 'Update Item Price',
+    '4': 'Update Item Quantity',
+    '5': 'Delete an Item',
+    '6': 'Delete All Items',
+    '7': 'Show Cart',
+    '8': 'Calculate Total',
+    '0': 'Exit'
+}
+
+def display_menu():
+    """ Display the cashier program menu. """
+    print("===== Cashier Program Menu =====")
+    
+    for key, value in MENU.items():
+        print(f"{key}. {value}")
+
+def input_item_name(prompt_text):
+    """Prompt the user to input an item name.
+
+    Parameters:
+    -----------
+    prompt_text : str
+        The prompt text for input.
+
+    Returns:
+    -----------
+    str: 
+        The item name entered by the user if the input is not empty.
+    """
+    item_name = input(prompt_text)
+
+    if input_not_empty(item_name):
+        return item_name.capitalize()
+    
+    else:
+        print("The input is empty. Please give the correct input")
+        return input_item_name(prompt_text)
+    
+def input_item_quantity(prompt_text):
+    """Prompt the user to input an item quantity.
+
+    Parameters:
+    -----------
+    prompt_text : str
+        The prompt text for input.
+
+    Returns:
+    -----------
+    str: 
+        The item quantity entered by the user if the input is not empty.
+    """
+    quantity = input(prompt_text)
+    if input_not_empty(quantity):
+        return quantity
+    
+    else:
+        print("The input is empty. Please give the correct input")
+        return input_item_quantity(prompt_text)
+    
+def input_item_price(prompt_text):
+    """Prompt the user to input an item price.
+
+    Parameters:
+    -----------
+    prompt_text : str
+        The prompt text for input.
+
+    Returns:
+    -----------
+    str: 
+        The item price entered by the user if the input is not empty.
+    """
+    price = input(prompt_text)
+    if input_not_empty(price):
+        return price
+    
+    else:
+        print("The input is empty or only contains empty space. Please give the correct input")
+        return input_item_price(prompt_text)
+
+def input_not_empty(value):
+    """Check if the input value is not empty or only contains empty space.
+
+    Parameters:
+    -----------
+    value : str
+        The input value to check.
+
+    Returns:
+    -----------
+    bool: 
+        True if the input value is not empty or only contains empty space, False otherwise.
+    """
+    if value.strip():
+        return True
+    
+    else:
+        return False
+
+def delete_confirmation(prompt_text):
+    """Asks for user confirmation to delete an/all item.
+
+    Parameters:
+    -----------
+    prompt_text : str
+        The text to be displayed as a prompt for confirmation.
+
+    Returns:
+    -----------
+    str: 
+        The user's choice ('y' for yes or 'n' for no) if the choice is y or n,
+    """
+    choice = input(f"Are you sure to delete {prompt_text}? (y/n) ").lower()
+
+    if choice=='y' or choice=='n':
+        return choice
+    
+    else:
+        print("Please input y or n")
+        return delete_confirmation(prompt_text)
+
+def main():
+    customer = Transaction()
+
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice in MENU:
+            if choice=='1':  # If the choice is 1, add an item to the customer's cart
+                print("=== Add an item to the cart ===")
+
+                item_name = input_item_name('Item Name: ')
+                quantity = input_item_quantity('Quantity: ')
+                price = input_item_price('Price: ')
+
+                print(customer.add_item(item_name, quantity, price))
+
+            elif choice=='2':  # If the choice is 2, update the name of an item in the cart
+                print("=== Update an item name ===")
+
+                old_item_name = input_item_name('Item name you want to change: ')
+                new_item_name = input_item_name('New Item Name: ')
+
+                print(customer.update_item_name(old_item_name, new_item_name))
+                
+            elif choice=='3':  # If the choice is 3, update the price of an item in the cart
+                print("=== Update an item price ===")
+
+                item_name = input_item_name('Item Name: ')
+                new_price = input_item_price('New Price: ')
+
+                print(customer.update_item_price(item_name, new_price))
+                
+            elif choice=='4':   # If the choice is 4, update the quantity of an item in the cart
+                print("=== Update an item quantity ===")
+
+                item_name = input_item_name('Item Name: ')
+                new_quantity = input_item_quantity('New Quantity: ')
+
+                print(customer.update_item_quantity(item_name, new_quantity))
+                
+            elif choice=='5':  # If the choice is 5, delete an item from the cart
+                print("=== Delete an item ===")
+                item_name = input_item_name("Item Name: ")
+                confirmation = delete_confirmation(item_name)
+
+                if confirmation == 'y':
+                    print(customer.delete_item(item_name))
+
+                else:
+                    print(f"Delete {item_name} cancelled")  # If choice is n (no), cancel the delete process
+                
+            elif choice=='6':   # If the choice is 6, reset the transaction (empty the cart)
+                print("=== Delete all items in the cart ===")
+                confirmation = delete_confirmation("all items")
+
+                if confirmation == 'y':
+                    print(customer.reset_transaction())
+
+                else:
+                    print("Delete all items cancelled") # If choice is n (no), cancel the delete process
+
+            elif choice=='7':  # If the choice is 7, display the cart
+                print("=== Display cart ===")
+                cart_contain = customer.check_order() 
+
+                if type(cart_contain) == dict:  #If the cart is not empty, print the transaction ID and the cart contents
+                    print(f"Transaction ID: {cart_contain['Transaction ID']}")
+                    print(f"{cart_contain['Cart']}")
+
+                else:  #If the cart is empty, print message
+                    print(cart_contain)
+
+            elif choice=='8':  # If the choice is 8, calculate the total price of the items in the cart
+                print("=== Display total ===")
+                total = customer.total_price()
+
+                if type(total) == dict:
+                    for key, value in total.items():
+                        print(f"{key}: {value}")
+
+                else:
+                    print(total)
+                    
+            elif choice=='0':  # If the choice is 0, exit the program
+                sys.exit()
+
+        else:
+            print("Invalid choice. Please try again.")
+
+        print()
+
+if __name__ == '__main__':
+    main()
---
+++ transaction.py
@@ -0,0 +1,353 @@
+import datetime
+import pandas
+
+class Transaction:
+    """Represents a transaction for a cashier program.
+
+    Attributes:
+    -----------
+    DISCOUNTS : dict
+        A dictionary that defines discount thresholds and corresponding discount percentages.
+        The keys represent the total cart value threshold, and the values represent the discount percentage.
+        The discounts are applied based on the total price of the items in the cart.
+        For example, if the total price exceeds 500000, a 10% discount will be applied.
+    id_transaction : str 
+        The ID of the transaction, generated based on the current date and time.
+    cart : pandas.DataFrame
+        The shopping cart containing the items in the transaction.
+
+    Methods
+    -----------
+    add_item(item_name, quantity, price)
+        Adds an item to the shopping cart.
+    update_item_name(old_item_name, new_item_name)
+        Updates the name of an item in the shopping cart.
+    update_item_quantity(item_name, new_quantity)
+        Updates the quantity of an item in the shopping cart.
+    update_item_price(item_name, new_price)
+        Updates the price of an item in the shopping cart.
+    delete_item(item_name)
+        Deletes an item from the shopping cart.
+    reset_transaction()
+        Resets the transaction by emptying the shopping cart.
+    get_item_index(item_name)
+        Retrieves the index of an item in the shopping cart.
+    check_order()
+        Displaying the transaction ID and the contents of the shopping cart.
+    total_price()
+        Calculates the total price of the items in the shopping cart, including any applicable discounts.
+    """
+
+    DISCOUNTS = {
+                500_000:10/100,
+                300_000:8/100,
+                200_000:5/100
+                } 
+
+    def __init__(self):
+        """  Initializes a new instance of the Transaction class.
+        Parameters:
+        -----------
+        id_transaction : str
+            The ID of the transaction, generated based on the current date and time.
+        cart : pandas.DataFrame
+            The shopping cart containing the items in the transaction.
+        """
+        self.id_transaction = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
+        self.cart = pandas.DataFrame(columns=[
+                                              "Item Name",
+                                              "Quantity",
+                                              "Price/Item",
+                                              "Total Price/Item"
+                                             ])
+
+    def add_item(self, item_name, quantity, price):
+        """ Adds a new item to the cart with the provided name, quantity, and price.
+        It calculates the total price for the item and appends it to the cart DataFrame.
+        If the item is already in the cart, it returns an appropriate message.
+
+        Parameters:
+        -----------
+        item_name : str
+            The name of the item.
+        quantity : float
+            The quantity of the item.
+        price : float
+            The price of the item.
+
+        Exceptions:
+        -----------
+        ValueError:
+            If the value of price or quantity can not convert into float.
+
+        Returns:
+        -----------
+        message : str
+            A message indicating the success or failure of adding the item to the cart.
+        """
+        index_item = self.get_item_index(item_name)
+        
+        if index_item.empty:
+            try:
+                quantity = float(quantity)
+
+                try:
+                    price = float(price)
+                    total_price_item = quantity * price
+
+                    item_data = [item_name, quantity, price, total_price_item]
+                    new_product = pandas.DataFrame([item_data], columns=self.cart.columns)
+                    self.cart = pandas.concat([self.cart, new_product])
+
+                    message = f"Item name: {item_name}, Quantity: {quantity}, Price: {price} is added to cart"
+                    return message
+                
+                except ValueError:
+                    message = "Price is not a number"
+                    return message
+                
+            except ValueError:
+                message ="Quantity of item is not a number"
+                return message
+            
+        else:
+            message = f"{item_name} already in the cart. Choose display to show your cart"
+            return message
+
+    def update_item_name(self, old_item_name, new_item_name):
+        """ Updates the name of an item in the cart from the old name 
+        to the new name if it is exist in the cart.
+        If the item does not exist in the cart, it returns an appropriate message.
+
+        Parameters:
+        -----------
+        old_item_name : str
+            The current name of the item.
+        new_item_name : str
+            The new name for the item.
+
+        Returns:
+        -----------
+        str: 
+            A message indicating whether the update was successful or an error message.
+        """
+        index_item = self.get_item_index(old_item_name)
+
+        if index_item.empty:
+            message = f"Product {old_item_name} does not exist"
+            return message
+        
+        else:
+            index_item = self.get_item_index(old_item_name)
+            self.cart.loc[index_item, 'Item Name'] = new_item_name
+
+            message = f"Update {old_item_name} to {new_item_name} success"
+            return message
+
+    def update_item_quantity(self, item_name, new_quantity):
+        """ Updates the quantity of an item in the cart to the new quantity if it is exist in the cart.
+        If the item does not exist in the cart, it returns an appropriate message.
+        If the new quantity is not a valid number, it returns an error message.
+
+        Parameters:
+        -----------
+        item_name : str
+            The name of the item.
+        new_quantity :float
+            The new quantity for the item.
+
+        Exceptions:
+        -----------
+        ValueError:
+            If the value of quantity can not convert into float.
+
+        Returns:
+        str: 
+            A message indicating whether the update was successful or an error message.
+        """
+        index_item = self.get_item_index(item_name)
+        if index_item.empty:
+            message = f"Product {item_name} does not exist"
+            return message
+            
+        else:
+            try:
+                new_quantity = float(new_quantity)
+
+                self.cart.loc[index_item, 'Quantity'] = new_quantity
+                price = self.cart.loc[index_item, 'Price/Item']
+                self.cart.loc[index_item, 'Total Price/Item'] = new_quantity * price
+
+                message = f"Update quantity of {item_name} success"
+                return message
+
+            except ValueError:
+                message = "Quantity is not a number"
+                return message
+
+    def update_item_price(self, item_name, new_price):
+        """ Updates the price of an item in the cart to the new price if it is exist in the cart.
+        If the item does not exist in the cart, it returns an appropriate message.
+        If the new price is not a valid number, it returns an error message.
+
+        Parameters:
+        -----------
+        item_name : str
+            The name of the item.
+        new_price : float
+            The new price for the item.
+
+        Exceptions:
+        -----------
+        ValueError:
+            If the value of quantity can not convert into float.
+
+        Returns:
+        -----------
+        str: 
+            A message indicating whether the update was successful or an error message.
+        """
+        index_item = self.get_item_index(item_name)
+        if index_item.empty:
+            message = f"Product {item_name} does not exist"
+            return message
+            
+        else:
+            try:
+                new_price = float(new_price)
+
+                self.cart.loc[index_item, 'Price/Item'] = new_price
+                quantity = self.cart.loc[index_item, 'Quantity']
+                self.cart.loc[index_item, 'Total Price/Item'] = quantity * new_price
+
+                message = f"Update price of {item_name} success"
+                return message
+
+            except ValueError:
+                message = "Price is not a number"
+                return message
+
+    def delete_item(self, item_name):
+        """ Removes an item from the cart based on the provided item name.
+        If the cart is empty or the item is not exist, it returns an appropriate message.
+        If the item is exist in the cart, it returns the item was successfully deleted
+
+        Parameters:
+        -----------
+        item_name : str
+            The name of the item.
+
+        Returns:
+        -----------
+        str: 
+            A message indicating whether the item was successfully deleted or an error message.
+        """
+
+        if self.cart.empty:
+            message = "Cart is empty"
+            return message
+        
+        else:
+            index_item = self.get_item_index(item_name)
+
+            if index_item.empty:
+                message = f"Product {item_name} does not exist"
+                return message
+            
+            else:
+                item_index = self.cart[self.cart["Item Name"] == item_name].index
+                self.cart = self.cart.drop(item_index)
+                self.cart = self.cart.reset_index(drop=True)
+
+                message = f"Product {item_name} is deleted"
+                return message
+          
+    def reset_transaction(self):
+        """ Resets the transaction by clearing the cart.
+        If the cart is empty, it returns an appropriate message.
+
+        Returns:
+        -----------
+        str: 
+            A message indicating whether the cart was successfully reset or an error message.
+        """
+        if self.cart.empty:
+            message = "Cart is empty"
+            return message
+        
+        else:
+            self.cart = self.cart.drop(self.cart.index)
+            message = "All items in Cart deleted"
+            return message
+
+    def get_item_index(self, item_name):
+        """ Retrieves the index of an item in the cart based on the item name.
+
+        Parameters:
+        -----------
+        item_name : str
+            The name of the item.
+
+        Returns:
+        -----------
+        pandas.Index: 
+            The index of the item in the cart DataFrame.
+        """
+        index_item = self.cart.loc[self.cart['Item Name'] == item_name].index
+        return index_item 
+   
+    def check_order(self):
+        """ This method checks the current order in the cart.
+        If the cart is empty, it returns an appropriate message.
+        If the cart is not empty, it returns the transaction ID and the cart contents.
+
+        Returns:
+        -----------
+        dict: 
+            A dictionary containing the transaction ID and the cart DataFrame if the cart is not empty.
+        str:
+            A message indicating whether the cart is empty.
+        """
+        if self.cart.empty:
+            message = "Cart is empty"
+            return message
+        
+        else:
+            self.cart = self.cart.reset_index(drop=True)
+            self.cart = self.cart.rename_axis('No')
+            self.cart.set_index(self.cart.index+1, inplace=True)
+            return {"Transaction ID" : self.id_transaction, 
+                    "Cart": self.cart}
+
+    def total_price(self):
+        """ Calculates the total price of the cart by summing the total price of each item.
+        It applies any applicable discounts based on the predefined DISCOUNTS dictionary.
+        It returns the total price, discount percentage, and total price after discount.
+        If the cart is empty, it returns an appropriate message.
+
+        Returns:
+        dict: 
+            A dictionary containing the total price, discount, and total price after discount.
+        str:
+            A message indicating whether the cart is empty.
+        """
+        if self.cart.empty:
+            message = "Cart is empty"
+            return message
+        
+        else:
+            total_shopping_cart = self.cart["Total Price/Item"].sum()
+
+            discount_percentage = 0
+            total_after_discount = total_shopping_cart
+
+            for threshold, discount in self.DISCOUNTS.items():
+                if total_shopping_cart > threshold:
+                    discount_percentage = discount
+                    total_after_discount = (total_shopping_cart
+                                            -(total_shopping_cart*discount_percentage))
+                    break
+            
+            return {'Total': f"Rp. {total_shopping_cart:,}", 
+                    'Discount': f"{int(discount_percentage*100)}%", 
+                    'Total after Discount': f"Rp. {total_after_discount:,}"}
